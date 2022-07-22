36783164971cb5bb2fbd74326e32ed21edec60dc
Alirio Mambo <alirio.mambo@flixbus.com>
2022-07-22 15:43:30 +0200

    feat: Optimize  Scoring utils resolves #10

--- app/tests/utils/test_scoring.py
+++ app/tests/utils/test_scoring.py
@@ -1,10 +1,18 @@
-from app.utils.scoring import generate_score
+from app.utils.scoring import generate_score, generate_score_all_grid
 
 
 def test_generate_score():
     grid = [[5, 3, 1, 0], [4, 1, 1, 3, 2], [2, 3, 2, 4, 3], [0, 2, 3, 3, 2], [1, 0, 2, 4, 3]]
     score = generate_score(grid, 1, 1)
     assert score == 22
+
+
+def test_generate_score_all_grid():
+    grid = [[5, 3, 1, 2, 0], [4, 1, 1, 3, 2], [2, 3, 2, 4, 3], [0, 2, 3, 3, 2], [1, 0, 2, 4, 3]]
+    score_list = generate_score_all_grid(grid)
+    for score in score_list:
+        score = score.dict()
+    assert len(score_list) == len(grid) * len(grid[0])
 
 
 def test_generate_score_edge():
--- app/utils/scoring.py
+++ app/utils/scoring.py
@@ -1,3 +1,23 @@
+from app.schemas.scores import Score
+from typing import List
+
+
+def sort_score_grid(score_grid: List[dict]) -> List[dict]:
+    return sorted(score_grid, key=lambda d: d['score'], reverse=True)
+
+
+def generate_score_all_grid(grid) -> List[Score]:
+    score_list = []
+
+    for x in range(0, len(grid)):
+        for y in range(0, len(grid[x])):
+            current_score = generate_score(grid, x, y)
+            score = Score(x=x, y=y, score=current_score)
+            score_list.append(score)
+
+    return score_list
+
+
 def generate_score(grid, x, y):
     heat_values_list = __get_grid_heat_values(grid, x, y)
     score = sum(heat_values_list)
